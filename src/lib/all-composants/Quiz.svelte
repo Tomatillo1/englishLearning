0b676a592d0e246f09d268adff59b176c42efe84
Tomatillo1 <totofarkas@gmail.com>
2025-11-30 16:25:01 +0100

    quiz design done

--- src/lib/all-composants/Quiz.svelte
+++ src/lib/all-composants/Quiz.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { X } from '@lucide/svelte';
+	import { Check } from '@lucide/svelte';
 	type Props = {
 		questionsList: {
 			questionNumber: number;
@@ -73,17 +75,35 @@
 			finalResult++;
 		}
 	}
-
 </script>
-<main class="flex flex-col justify-center items-center w-full">
-	<p class="font-bold">{howManyQuestionsDone}/10</p>
-	<h2 class="text-center">{displayTheQuestion.caption}</h2>
-	<section class="flex flex-col w-full gap-4 pt-4 items-center">
+<header class="flex flex-col w-full">
+	<div class="flex flex-row items-center text-red-800 w-full justify-between">
+		<h1 class="text-3xl font-bold text-sky-950">Quiz</h1>
+		<p class="font-bold text-xl">{howManyQuestionsDone}/10</p>
+	</div>
+	<div class="mt-4 w-full h-4 rounded-full bg-gray-200 overflow-hidden">
+		<div class="h-full rounded-full bg-red-700 overflow-hidden" style="width: {howManyQuestionsDone * 10}%"></div>
+	</div>
+</header>
+<main class="h-screen flex flex-col w-full justify-center pt-8 pb-12">
+<section class="flex flex-col px-8 bg-white border-2 border-gray-100 rounded-2xl h-full justify-center">
+	<h2 class="text-center text-xl font-bold">{displayTheQuestion.caption}</h2>
+	<div class="flex flex-col w-full gap-4 pt-4 items-center">
 		{#each displayTheQuestion.answers as button (button.answer)}
-			<button onclick={() => onAnswerClicked(button)} disabled={selectedAnswer !== undefined} class="border-2 border-black rounded-full w-2/5" class:bg-green-500={selectedAnswer?.answer === button.answer && button.boolean} class:bg-red-500={selectedAnswer?.answer === button.answer && !button.boolean} class:text-green-500={selectedAnswer !== undefined && selectedAnswer?.answer !== button.answer && button.boolean}>{button.answer}</button>
+			<button onclick={() => onAnswerClicked(button)} disabled={selectedAnswer !== undefined} class="flex flex-row justify-between items-center border-2 border-gray-100 rounded-2xl w-full text-start py-4 px-4 text-lg" class:bg-green-200={selectedAnswer?.answer === button.answer && button.boolean} class:border-green-500={selectedAnswer?.answer === button.answer && button.boolean} class:bg-red-500={selectedAnswer?.answer === button.answer && !button.boolean} class:text-white={selectedAnswer?.answer === button.answer && !button.boolean} class:border-green-400={selectedAnswer !== undefined && selectedAnswer?.answer !== button.answer && button.boolean} class:bg-green-100={selectedAnswer !== undefined && selectedAnswer?.answer !== button.answer && button.boolean}>
+				{button.answer}
+				{#if selectedAnswer?.answer === button.answer && !button.boolean}
+					<X class="stroke-white border-1 border-white rounded-full p-1"/>
+				{:else if selectedAnswer?.answer === button.answer && button.boolean}
+					<Check class="stroke-green-500 border-1 border-green-500 rounded-full p-1"/>
+				{:else if selectedAnswer !== undefined && selectedAnswer?.answer !== button.answer && button.boolean}
+					<Check class="stroke-green-500 border-1 border-green-500 rounded-full p-1"/>
+				{/if}
+				</button>
 		{/each}
-		<button onclick={nextQuestion} disabled={selectedAnswer === undefined} class="border-2 border-black italic rounded-full font-bold w-1/2">{howManyQuestionsDone === 10 ? "Finish" : "Next"}</button>
-	</section>
+		<button onclick={nextQuestion} disabled={selectedAnswer === undefined} class="mt-2 bg-sky-950 text-white italic rounded-lg font-bold w-2/3 py-2">{howManyQuestionsDone === 10 ? "Finish" : "Next"}</button>
+	</div>
+</section>
 </main>
 <style>
 
--- src/routes/lessons/kitchen-utensils/quiz/+page.svelte
+++ src/routes/lessons/kitchen-utensils/quiz/+page.svelte
@@ -2,30 +2,26 @@
 	import Quiz from '$lib/all-composants/Quiz.svelte';
 	import { lessonKitchenUtensils } from '$lib/all-my-lessons-datas/kitchen-utensils';
 
-	const kitchenQuestions = lessonKitchenUtensils.lessonQuestions
+	const kitchenQuestions = lessonKitchenUtensils.lessonQuestions;
 	let displayResult: number | undefined = $state(undefined);
 	const onQuizFinished = (result: number) => displayResult = result;
 </script>
 
-<main class="h-screen flex flex-col">
-	<h1>Quiz</h1>
-	{#if displayResult !== undefined}
+
+{#if displayResult !== undefined}
+	<main class="h-screen flex flex-col">
 		<section class="flex flex-col justify-center items-center h-full pb-8 gap-3 ">
 			<h2 class="text-lg font-bold underline">Results</h2>
 			<p class="text-base">Your result is :</p>
 			<p class="font-bold italic border-2 px-4 py-1">{displayResult}/10</p>
-			<button onclick={displayResult = undefined} class="border-2 border-black px-4 py-1 italic rounded-full">Do you want to try again ?</button>
+			<button onclick={displayResult = undefined} class="border-2 border-black px-4 py-1 italic rounded-full">Do you
+				want to try again ?
+			</button>
 		</section>
-	{:else}
-	<section class="flex flex-col justify-center items-center h-full pb-8">
-		<Quiz questionsList={kitchenQuestions} onQuizFinished={onQuizFinished}/>
-	</section>
-	{/if}
-</main>
-<style>
-    h1 {
-        text-align: center;
-        padding-top: 1rem;
-        font-size: 1.5rem;
-    }
-</style>
+	</main>
+{:else}
+	<main class="h-screen flex flex-col w-full px-4 pt-4 ">
+			<Quiz questionsList={kitchenQuestions} onQuizFinished={onQuizFinished} />
+	</main>
+{/if}
+
