7f932c244e401f23e40512aa6d89f8c32d03559b
Tomatillo1 <totofarkas@gmail.com>
2025-12-03 11:42:21 +0100

    add quiz dynamic route params

--- src/lib/all-my-lessons-datas/allLessons.ts
+++ src/lib/all-my-lessons-datas/allLessons.ts
@@ -1,14 +1,15 @@
-import type { LessonsPlan } from '$lib/interfaces/lessons-plan';
 import { lessonBathroomItems } from '$lib/all-my-lessons-datas/bathroom';
 import { lessonKitchenObjects } from '$lib/all-my-lessons-datas/kitchen';
 import { lessonBedroomItems } from '$lib/all-my-lessons-datas/bedroom';
 import { lessonKitchenUtensils } from '$lib/all-my-lessons-datas/kitchen-utensils';
 import { lessonLivingRoom } from '$lib/all-my-lessons-datas/livingroom';
 
-export const allLessons	: Record<string, LessonsPlan> = {
+export const allLessons = {
 	bathroom: lessonBathroomItems,
 	kitchen: lessonKitchenObjects,
 	bedroom: lessonBedroomItems,
-	"kitchen-utensils": lessonKitchenUtensils,
-	livingroom: lessonLivingRoom,
-}
+	'kitchen-utensils': lessonKitchenUtensils,
+	livingroom: lessonLivingRoom
+};
+
+export type Theme = keyof typeof allLessons;
--- src/routes/lessons/[theme]/+page.svelte
+++ src/routes/lessons/[theme]/+page.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-	import { allLessons } from '$lib/all-my-lessons-datas/allLessons';
+	import { allLessons, type Theme } from '$lib/all-my-lessons-datas/allLessons';
 	import DisplayLessonsWords from '$lib/all-composants/DisplayLessonsWords.svelte';
 	import ButtonLinkToQuizz from '$lib/all-composants/ButtonLinkToQuiz.svelte';
 	import { House } from '@lucide/svelte';
 	import { resolve } from '$app/paths';
 	import type { PageProps } from './$types';
+	import { error } from '@sveltejs/kit';
 
 	let props: PageProps = $props();
 
-	// Todo : change variable name
-	const wordsList = allLessons[props.params.theme]?.lessonWords;
+	let safeTheme: Theme | undefined = $state(undefined);
 
+	const allTheme: Theme[] = Object.keys(allLessons) as Theme[];
+	if (allTheme.find((item) => props.params.theme === item)) {
+		safeTheme = props.params.theme as Theme;
+	} else {
+		error(404, 'Theme does not exist');
+	}
+
+	const wordsList = allLessons[safeTheme ?? 'bathroom']?.lessonWords;
 </script>
+
 <header class="flex flex-col bg-sky-950 text-gray-50 pb-6 pt-4 relative">
-	<a href={resolve("/")}>
+	<a href={resolve('/')}>
 		<House class="absolute top-3 left-3" />
 	</a>
 	<section class="flex flex-col items-center">
@@ -24,9 +33,10 @@
 </header>
 <main class="h-screen flex flex-col w-full relative pt-4">
 	<!-- Todo : change link to quiz -->
-	<ButtonLinkToQuizz linkToQuiz="/lessons/kitchen-utensils/quiz" />
+	{#if safeTheme}
+		<ButtonLinkToQuizz linkToQuiz="/lessons/{safeTheme}/quiz" />
+	{/if}
 	<section class="w-full px-4 pb-20">
-		<!-- Todo : change new variable name -->
-		<DisplayLessonsWords wordsList={wordsList} />
+		<DisplayLessonsWords {wordsList} />
 	</section>
 </main>
