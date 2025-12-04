ffc8e1169765c3c1c29504611e4b75a22c4abd0c
Tomatillo1 <totofarkas@gmail.com>
2025-12-04 20:15:46 +0100

    add difficulty

--- src/lib/interfaces/lessons-display.ts
+++ src/lib/interfaces/lessons-display.ts
@@ -14,31 +14,64 @@ interface Lesson {
 	link: AllLinks;
 	imageLink: string;
 	name: string;
+	difficulty: string;
 }
 
-// Todo : add lesson homepage
 export const lessonDisplay: Lesson[] = [
 	{
 		id: 1,
 		link: '/lessons/kitchen-utensils',
 		imageLink: '/lessons-images/kitchen-utensils.webp',
-		name: 'Kitchen utensils'
+		name: 'Kitchen utensils',
+		difficulty: 'Beginners'
 	},
-	{ id: 2, link: '/lessons/bedroom', imageLink: '/lessons-images/bedroom.webp', name: 'Bedroom' },
+	{
+		id: 2,
+		link: '/lessons/bedroom',
+		imageLink: '/lessons-images/bedroom.webp',
+		name: 'Bedroom',
+		difficulty: 'Beginners'
+	},
 	{
 		id: 3,
 		link: '/lessons/bathroom',
 		imageLink: '/lessons-images/bathroom.webp',
-		name: 'Bathroom'
+		name: 'Bathroom',
+		difficulty: 'Beginners'
 	},
 	{
 		id: 4,
 		link: '/lessons/livingroom',
 		imageLink: '/lessons-images/livingroom.webp',
-		name: 'Living room'
+		name: 'Living room',
+		difficulty: 'Beginners'
 	},
-	{ id: 5, link: '/lessons/kitchen', imageLink: '/lessons-images/kitchen.webp', name: 'Kitchen' },
-	{ id: 6, link: '/lessons/garden', imageLink: '/lessons-images/garden.webp', name: 'Garden' },
-	{ id: 7, link: '/lessons/clothes', imageLink: '/lessons-images/clothes.webp', name: 'Clothes' },
-	{ id: 8, link: '/lessons/weather', imageLink: '/lessons-images/weather.webp', name: 'Weather' }
+	{
+		id: 5,
+		link: '/lessons/kitchen',
+		imageLink: '/lessons-images/kitchen.webp',
+		name: 'Kitchen',
+		difficulty: 'Beginners'
+	},
+	{
+		id: 6,
+		link: '/lessons/garden',
+		imageLink: '/lessons-images/garden.webp',
+		name: 'Garden',
+		difficulty: 'Beginners'
+	},
+	{
+		id: 7,
+		link: '/lessons/clothes',
+		imageLink: '/lessons-images/clothes.webp',
+		name: 'Clothes',
+		difficulty: 'Beginners'
+	},
+	{
+		id: 8,
+		link: '/lessons/weather',
+		imageLink: '/lessons-images/weather.webp',
+		name: 'Weather',
+		difficulty: 'Beginners'
+	}
 ];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,15 @@
 	import { lessonDisplay } from '$lib/interfaces/lessons-display';
 	import { Star } from '@lucide/svelte';
 	import { ChevronRight } from '@lucide/svelte';
-
 </script>
 
 <header class="flex flex-col bg-sky-950 text-gray-50 gap-2 rounded-b-4xl pb-6">
 	<section class="flex flex-row gap-4 pt-4 pl-4 items-center">
-		<img class="aspect-square object-cover w-1/6 rounded-full overflow-hidden" src="/assets/favicon-el.png"
-				 alt="logo-english-learning">
+		<img
+			class="aspect-square object-cover w-1/6 rounded-full overflow-hidden"
+			src="/assets/favicon-el.png"
+			alt="logo-english-learning"
+		/>
 		<div class="flex flex-col">
 			<h1 class="text-3xl font-bold">EnglishLearning</h1>
 			<p class="italic text-gray-300">Master new vocabulary today</p>
@@ -20,14 +22,23 @@
 	<section class="flex flex-col gap-4 w-full px-4">
 		<h2 class="text-start font-bold text-sky-950 text-lg">Learning Paths</h2>
 		{#each lessonDisplay as lesson (lesson.id)}
-			<a class="flex flex-row gap-4 px-4 py-4 bg-white border-2 border-gray-100 rounded-2xl items-center" href={resolve(lesson.link)}>
-				<img class=" w-1/4 aspect-square object-cover overflow-hidden rounded-2xl" src="{lesson.imageLink}" alt="">
+			<a
+				class="flex flex-row gap-4 px-4 py-4 bg-white border-2 border-gray-100 rounded-2xl items-center"
+				href={resolve(lesson.link)}
+			>
+				<img
+					class=" w-1/4 aspect-square object-cover overflow-hidden rounded-2xl"
+					src={lesson.imageLink}
+					alt=""
+				/>
 				<div class="flex flex-col gap-2">
 					<h2 class="text-lg text-sky-950 font-bold">{lesson.name}</h2>
 					<div class="flex flex-row gap-2 items-center">
-					<p class="bg-gray-100 rounded-lg px-3 py-1 w-fit font-bold text-gray-400 text-xs">20 words</p>
-						<Star class="fill-yellow-400 stroke-yellow-400 w-4"/>
-						<p class="font-bold text-gray-400 text-xs">Beginners</p>
+						<p class="bg-gray-100 rounded-lg px-3 py-1 w-fit font-bold text-gray-400 text-xs">
+							20 words
+						</p>
+						<Star class="fill-yellow-400 stroke-yellow-400 w-4" />
+						<p class="font-bold text-gray-400 text-xs">{lesson.difficulty}</p>
 					</div>
 				</div>
 				<ChevronRight class="stroke-gray-400 bg-gray-100 rounded-full" />
